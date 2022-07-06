5bb409568d2f3264aa21a1d5818b70e7e04a0a4b
Ksenia Yunknickel <ksenia.yunknickel@omega-r.com>
2022-07-06 11:48:38 +0300

    Change logic

--- app/src/main/java/com/kesimilim/newsmap/fragment/WelcomeFragment.kt
+++ app/src/main/java/com/kesimilim/newsmap/fragment/WelcomeFragment.kt
@@ -2,26 +2,65 @@ package com.kesimilim.newsmap.fragment
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
+import androidx.activity.result.ActivityResultLauncher
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.kesimilim.newsmap.R
+import com.kesimilim.newsmap.activity.HomeActivity
 import com.kesimilim.newsmap.databinding.FragmentWelcomeBinding
 import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
+import com.vk.api.sdk.exceptions.VKAuthException
 
 class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
 
+    private lateinit var authLauncher: ActivityResultLauncher<Collection<VKScope>>
     private lateinit var binding: FragmentWelcomeBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         if (VK.isLoggedIn()) {
             findNavController().navigate(R.id.action_welcomeFragment_to_homeFragment)
-//            HomeActivity.startFrom(this)
-//            finish()
-            return
         }
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentWelcomeBinding.bind(view)
 
+        authLauncher = VK.login() { result : VKAuthenticationResult ->
+            when (result) {
+                is VKAuthenticationResult.Success -> onLogin()
+                is VKAuthenticationResult.Failed -> onLoginFailed(result.exception)
+            }
+        }
+
+        binding = FragmentWelcomeBinding.bind(view)
+        binding.loginBtn.setOnClickListener {
+            authLauncher.launch(arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+        }
+
+    }
+
+    private fun onLogin() {
+        findNavController().navigate(R.id.action_welcomeFragment_to_homeFragment)
+
+    }
+
+    private fun onLoginFailed(exception: VKAuthException) {
+        if (!exception.isCanceled) {
+            val descriptionResourse =
+                if (exception.webViewError == WebViewClient.ERROR_HOST_LOOKUP) R.string.message_connection_error
+                else R.string.message_unknown_error
+
+            AlertDialog.Builder(this)
+                .setMessage(descriptionResourse)
+                .setPositiveButton(com.vk.api.sdk.R.string.vk_retry) { _, _ ->
+                    authLauncher.launch(arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+                }
+                .setNegativeButton(android.R.string.cancel) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .show()
+        }
     }
 
 }
