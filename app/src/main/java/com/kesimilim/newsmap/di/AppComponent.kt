372694b55f119fb03cf4c1a5ddf12c46839aa9e8
Ksenia Yunknickel <ksenia.yunknickel@omega-r.com>
2022-07-25 14:34:25 +0300

    Fix clean database

--- app/src/main/java/com/kesimilim/newsmap/di/AppComponent.kt
+++ app/src/main/java/com/kesimilim/newsmap/di/AppComponent.kt
@@ -7,6 +7,7 @@ import com.kesimilim.newsmap.screens.maps.google.GoogleActivity
 import com.kesimilim.newsmap.screens.maps.yandex.YandexActivity
 import com.kesimilim.newsmap.screens.main.MainActivity
 import com.kesimilim.newsmap.screens.wall.WallActivity
+import com.kesimilim.newsmap.screens.welcome.WelcomeActivity
 import dagger.Component
 import javax.inject.Singleton
 
@@ -19,4 +20,5 @@ interface AppComponent {
     fun inject(yandexActivity: YandexActivity)
     fun inject(googleActivity: GoogleActivity)
     fun inject(wallActivity: WallActivity)
+    fun inject(welcomeActivity: WelcomeActivity)
 }
